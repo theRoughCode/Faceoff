735470dc410d0c9b3989707875f10c72c361dabb
deadsupra <paulrdelgado@gmail.com>
2017-09-23 08:51:26 -0700

    added server logging, compression support, icon response

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./routes');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const compression = require('compression');
 
 // Enable hiding of sensitive information
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 
 //  Connect all our routes to our application
+app.use(compression());
+app.use(favicon(__dirname + 'resources/logo.ico'));
+app.use(logger('dev'));
 app.use('/', routes);
 
 // Turn on that server!
