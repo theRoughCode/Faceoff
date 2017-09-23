97cc2fb9ecf9084895db700381d5d538df64d9a8
deadsupra <paulrdelgado@gmail.com>
2017-09-23 15:26:49 -0700

    added https server feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // grab the packages we need
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -7,12 +8,17 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const compression = require('compression');
 
+const http = require('http');
+const hs = require('https');
+const sockio = require('socket.io');
+
 // Import the Anagrammatix game file.
 var faceoff = require('./helpers/faceoff');
 
 // Enable hiding of sensitive information
 require('dotenv').config();
 const port = process.env.PORT || 8000;
+const p2 = 9000;
 
 //  Connect all our routes to our application
 app.use(compression());
@@ -28,13 +34,23 @@ app.use(express.static(path.join(__dirname, '/views')));
 app.set('view engine', 'ejs');
 
 // Turn on that server!
-const server = require('http').createServer(app).listen(port, () => console.log('Server started! At http://localhost:' + port));
+var pkey = fs.readFileSync('server.key', 'utf8');
+var cert = fs.readFileSync('server.crt', 'utf8');
+var cred = {key: pkey, cert: cert};
+const server = http.createServer(app).listen(port, () => console.log('Server started! At port:' + port));
+const s2 = hs.createServer(cred, app).listen(p2, () => console.log('Https Server listening on:' + p2));
 
 // Create a Socket.IO server and attach it to the http server
-var io = require('socket.io').listen(server);
+var io = sockio.listen(server);
+var ios = sockio.listen(s2);
 
 // Listen for Socket.IO Connections. Once connected, start the game logic.
 io.sockets.on('connection', function (socket) {
+    console.log('client connected');
+    faceoff.initGame(io, socket);
+});
+
+ios.sockets.on('connection', function (socket) {
     console.log('client connected');
     faceoff.initGame(io, socket);
 });
