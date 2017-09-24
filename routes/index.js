1039921bec2e7faf987aa6caafc8769fc0f2fc31
deadsupra <paulrdelgado@gmail.com>
2017-09-23 23:39:55 -0700

    added other index ejs to route response

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ routes.get('/', function(req, res){
   res.render('index3');
 });
 
+routes.get('/index2', function(req, res){
+  res.render('index2');
+});
+
 routes.get('/fb', function(req, res){
 	res.render('index');
 });
