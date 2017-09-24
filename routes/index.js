099e6318f4ea570b1c9bb5fa25f02a3033af4fe9
raphael <raph.koh@gmail.com>
2017-09-23 22:29:16 -0400

    Update FE

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,7 @@
 const routes = require('express').Router();
 
 routes.get('/', function(req, res){
-  res.render('index3', {
-    template:`../views/partials/intro.ejs`
-  });
-});
-
-routes.get('/:template', function(req, res){
-  res.render('index3', {
-    template:`../views/partials/${req.params.template}.ejs`
-  });
+  res.render('index3');
 });
 
 routes.get('/fb', function(req, res){
