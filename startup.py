fee0db53f4195e1836c045cde40355fbd7966fd5
deadsupra <paulrdelgado@gmail.com>
2017-09-23 21:53:26 -0700

    add export for emotion key

--- startup.py
+++ startup.py
@@ -4,6 +4,8 @@ import subprocess
 # Port 2: 9000
 cmd1 = "sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 9000"
 cmd2 = "sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 9000"
+cmd3 = "export EMOTION_KEY=9f539c1c51574a29be83cc18c62f6b07"
 
 subprocess.call([cmd1], shell=True)
 subprocess.call([cmd2], shell=True)
+subprocess.call([cmd3], shell=True)
